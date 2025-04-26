e75a80db52fb88abb5f153073dca23d948c64454
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-04-25 19:28:08 -0500

    Corregidos errores eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background">
       <h1 className='text-3xl font-bold text-yellow-300 mb-4'>

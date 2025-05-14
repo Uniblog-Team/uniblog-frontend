d5943495e5a7e0a0931f45d1693cee548b31b4d0
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-05-14 00:25:11 -0500

    Elementos de sonarqube y jest agregados

---
+++ src/test/HelloWorld.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import HelloWorld from '../components/HelloWorld';
+
+
+describe('HelloWorld Component', () => {
+    test('renders hello world message', () => {
+      render(<HelloWorld />);
+
+      expect(screen.getByText('Hello, World!')).toBeInTheDocument();
+    });
+  });

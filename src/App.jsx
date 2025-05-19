00fffa39e0e0877321d9750a8389cdd33c82ce56
alexnworldl <alexgarzon0929@gmail.com>
2025-05-19 09:18:49 -0500

    Loginpage/Nuevasconfiguraciones

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Button } from '@mui/material'
 
 function App() {
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background">
       <h1 className='text-3xl font-bold text-yellow-300 mb-4'>

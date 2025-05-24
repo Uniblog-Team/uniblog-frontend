a458fe921ac93b100607e20a04c2c3553e553124
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-05-23 19:56:35 -0500

    cambios para aprobar eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background">
